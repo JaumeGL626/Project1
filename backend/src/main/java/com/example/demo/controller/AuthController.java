991039eaffd9974a8d42d429ce0e39196180ffd3
Jaume G <u1996070@campus.udg.edu>
2026-07-02 07:52:17 +0100

    Implemented login endPoint

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,14 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.LoginRequest;
-import com.example.demo.entity.User;
+import com.example.demo.dto.LoginResponse;
 import com.example.demo.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(path = "/api/auth")
@@ -17,6 +15,12 @@ public class AuthController {
 
     public AuthController (AuthService authService){
         this.authService=authService;
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity <LoginResponse> login(@Valid @RequestBody LoginRequest request){
+        LoginResponse loginResponse = authService.login(request);
+        return  ResponseEntity.ok(loginResponse);
     }
 
 
