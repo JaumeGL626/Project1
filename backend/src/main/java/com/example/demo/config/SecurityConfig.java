3dfdb8a657f888b9b2154335ac79c12cd58ccff5
Jaume G <u1996070@campus.udg.edu>
2026-07-03 03:10:59 +0100

    Implemented CustomUserdetails

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //everyOne Can login
         http
                 .csrf(csrf-> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
