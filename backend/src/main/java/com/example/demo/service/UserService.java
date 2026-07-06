3aeadcf784c00a5f84f046186ec9c23e58f6a775
Jaume G <u1996070@campus.udg.edu>
2026-07-06 07:09:56 +0100

    Implemented getPublicProfile UserService

--- backend/src/main/java/com/example/demo/service/UserService.java
+++ backend/src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.dto.UserProfileDto;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.security.JwtService;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,23 +11,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
 
+    private final JwtService jwtService;
     private final UserRepository userRepository;
 
 
-    public UserService (UserRepository userRepository){
-
+    public UserService (UserRepository userRepository, JwtService jwtService){
+        this.jwtService=jwtService;
         this.userRepository=userRepository;
     }
     @Transactional (readOnly = true)
-    public UserProfileDto getProfile (Long userId){
+    public UserProfileDto getPublicProfile (Long userId){
         User user= userRepository.findById(userId).orElseThrow(()-> new ServiceException("Usuari inexistent"));
-        return new UserProfileDto(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getDescription(),
-                user.getRole()
-        );
+        return userMapper.userToUserProfileDto(user);
+    }
+
+    public UserProfileDto getProfile(String token){
+        String username =jwtService.extractUsername(token);
+        User user= userRepository.findByEmail(username).orElseThrow(()-> new RuntimeException("usari inexistent"));
+        return new userMapper.userToUserProfileDto(user);
+
 
     }
 
