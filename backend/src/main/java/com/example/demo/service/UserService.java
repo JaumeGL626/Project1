cd179b8d2503c18ecf3746546860ed9c9eb85a52
Jaume G <u1996070@campus.udg.edu>
2026-07-06 06:36:50 +0100

    Implemented getprofile UserService

--- backend/src/main/java/com/example/demo/service/UserService.java
+++ backend/src/main/java/com/example/demo/service/UserService.java
@@ -1,15 +1,34 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UserProfileDto;
+import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
 
+
     public UserService (UserRepository userRepository){
+
         this.userRepository=userRepository;
     }
+    @Transactional (readOnly = true)
+    public UserProfileDto getProfile (Long userId){
+        User user= userRepository.findById(userId).orElseThrow(()-> new ServiceException("Usuari inexistent"));
+        return new UserProfileDto(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getDescription(),
+                user.getRole()
+        );
+
+    }
+
 
 }
