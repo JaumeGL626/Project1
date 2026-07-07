cb8d58d495227e1a7e3ecd178ef1e8e36afcde91
Jaume G <u1996070@campus.udg.edu>
2026-07-07 02:45:57 +0100

    Added ProfilePicture to Home

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,7 +1,27 @@
+import { CircleUser} from 'lucide-react'
+import { useNavigate } from "react-router-dom";
+import '../styles/home-page.css'
+
 function HomePage() {
+
+    const navigate = useNavigate();
+
+    function handleUserProfie(){
+        navigate("/profile")
+    }
+
+
     return (
-        <h1>Home</h1>
-    );
+         <>
+            <header>
+                <h2>Home</h2>
+                <div className="iconUserCircle">
+                    <CircleUser onClick={handleUserProfie}/>
+
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default HomePage;
