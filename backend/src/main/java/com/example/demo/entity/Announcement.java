4b2cff01cb510875597f595cee4c2434a31617e8
Jaume G <u1996070@campus.udg.edu>
2026-07-18 07:49:16 +0100

    Implemented Announcement Entity

---
+++ backend/src/main/java/com/example/demo/entity/Announcement.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table (name="announcements")
+public class Announcement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ElementCollection
+    @CollectionTable(name = "announcement_urls", joinColumns = @JoinColumn(name = "announcement_id"))
+    @Column(name = "url")
+    private List <String> urlAnnouncements;
+    private LocalDateTime date;
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+
+}
