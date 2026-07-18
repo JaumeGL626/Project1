79a7ae6de62e374229784007d0aacccc9e1b2692
Jaume G <u1996070@campus.udg.edu>
2026-07-18 01:42:08 +0100

    Implemented function editProfile

--- frontend/src/pages/userProfile-page.jsx
+++ frontend/src/pages/userProfile-page.jsx
@@ -2,11 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { CircleUser} from 'lucide-react'
 import "../styles/userProfile-page.css";
 import { CurrentUserContex } from "../components/userContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 function UserProfilePage(){
 
    const navigate=useNavigate();
-   const {user,loading,updateUser}=useContext(CurrentUserContex);   
+   const [editing,setEditing]=useState(false);
+   const [error,setError]=useState("");
+   const {user,loading,updateUser}=useContext(CurrentUserContex); 
+   const [username, setUsername] = useState("");
+   const [description, setDescription] = useState("");  
 
     if(loading){
         return <>
@@ -14,13 +18,52 @@ function UserProfilePage(){
         </>
     }
 
+
+
    function handleLogout(){
     localStorage.removeItem("token");
     updateUser("");
 
     navigate("/login")
+   }
+
+   function handleEdit(){
+    setEditing(true);
+    setUsername(user.username);
+    setDescription(user.description);
+   }
+   function handleCancel(){
+    setEditing(false);
+   }
+   function handleSave(){
+    const token =localStorage.getItem("token");
+    const requestOptions={
+        method: "PUT",
+
+        headers: {
+            Authorization: `Bearer ${token}` ,
+             "Content-Type": "application/json"},
+         body: JSON.stringify({
+                username:username, 
+                description:description,
+        })
+    };
+    fetch("http://localhost:8080/api/users/me", requestOptions)
+   .then(response=> {
+    if(!response.ok){
+        throw new Error ("error al editar perfil")
+    }
+    return response.json();
+   })
+   .then(response=> {
+    updateUser(response);
+    setEditing(false);
+   })
+   .catch(error=> setError(error.message));
 
    }
+
+
    
 
 
@@ -45,14 +88,31 @@ function UserProfilePage(){
                         <CircleUser size={200} />
                     )}
 
-                    <p className="user-name"><strong> {user.username}</strong> </p>
+                    {editing ? (
+                        <div className="edit-form">
+                            <label> Username:</label>
+                            <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <label>Descripcio:</label>
+                            <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                            <div className="editButtons">
+                                <button className="cancelbutton" onClick={handleCancel}>Cancelar </button>
+                                <button className="ssaveButton" onClick={handleSave}> Guardar</button>
+
+                            </div>
+                        </div>
+                    ): (
+                        <>
+                            <p className="user-name"><strong> {user.username}</strong> </p>
                             <p className="user-email"> {user.email} </p>
                             <p className='user-study'> Grau Estudi:</p>
                             <p className='user-description'>{user.description} </p>
                             <p className='user-role'>{user.role} </p>
 
-                            <button className='editProfile'> Editar Perfil</button>
+                            <button className='editProfile' onClick={handleEdit}> Editar Perfil</button>
                             <button className='logout' onClick={handleLogout}> Tancar Sessio</button>
+                        </>
+                        
+                    )}
 
                 </section>
 
