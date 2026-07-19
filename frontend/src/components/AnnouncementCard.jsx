0f5c9b26402b2293179b20cdf4f427d6f5c4609d
Jaume G <u1996070@campus.udg.edu>
2026-07-19 01:59:02 +0100

    Implemented announcementCard

---
+++ frontend/src/components/AnnouncementCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const AnnouncementCard =({announcement})=> {
+const {urlPhotos, date, description, owner, ownerprofilePicture,ownerId,title, id}
+
+    return(
+        <div className="Announcementcard">
+            <div clas
+
+        </div>
+    )
+
+}
