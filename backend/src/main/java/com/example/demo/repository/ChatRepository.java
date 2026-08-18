6418e6c366c381609f9f4da61c334c737a37c1fc
unknown <u1996070@campus.udg.edu>
2026-08-18 15:20:35 +0200

    Implemented ChatRepository

--- backend/src/main/java/com/example/demo/repository/ChatRepository.java
+++ backend/src/main/java/com/example/demo/repository/ChatRepository.java
@@ -1,4 +1,17 @@
 package com.example.demo.repository;
 
-public interface ChatRepository {
+import com.example.demo.entity.Chat;
+import com.example.demo.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRepository  extends JpaRepository<Chat,Long> {
+
+    Optional <Chat> findById(Long id);
+    List<Chat> findByName(String name);
+    List<Chat> findBySubForumIdOrderByNameAsc(Long id);
+    List<Chat>findByUserId(Long id);
+
 }
