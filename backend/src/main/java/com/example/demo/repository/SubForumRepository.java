085313bc55aa53436a74faaf995bf4e84ebda38e
unknown <u1996070@campus.udg.edu>
2026-08-20 16:43:18 +0200

    Implemented Forum repository

--- backend/src/main/java/com/example/demo/repository/SubForumRepository.java
+++ backend/src/main/java/com/example/demo/repository/SubForumRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface SubForumRepository extends JpaRepository<SubForum,Long> {
 
-    //List<SubForum> findBuForumId(Long id);
+    List<SubForum> findByForumId(Long id);
     boolean existsByName(String name);
 }
