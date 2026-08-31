d95a9cbf182b7e6929b53920ce140fca05cc0704
unknown <u1996070@campus.udg.edu>
2026-08-31 17:25:19 +0200

    forumService implemented

---
+++ frontend/src/services/forumService.js
@@ -0,0 +1,21 @@
+import { apiClient } from '../api/apiClient';
+
+
+export const forumService={
+    getAllForums: async ()=>{
+        return await apiClient("/forums");
+    },
+
+    postForum: async (name, description)=>{
+        return await apiClient ("/forums",{
+            method: "POST",
+            body: JSON.stringify({ 
+                    description: description,
+                    name:name
+                })
+        })
+    },
+    getForumById: async (id) => {
+    return await apiClient(`/forums/${id}`);
+  },
+};
