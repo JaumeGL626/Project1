1aae31ed2af8407094af4268811c321a3302d2df
unknown <u1996070@campus.udg.edu>
2026-09-04 18:59:31 +0200

    third version of specific-Forum page plus style

--- frontend/src/pages/specificForum-page.jsx
+++ frontend/src/pages/specificForum-page.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { forumService } from "../services/forumService";
 import { useParams } from "react-router-dom"
 import { useEffect } from "react";
+import '../styles/specificForumStyle.css'
 function SpecificForumPage(){
     const [forum,setForum]=useState(null);
     const { id } = useParams();
@@ -43,64 +44,72 @@ function SpecificForumPage(){
     return(
     <>
     
-    
-    <div className="subForums">
-         <h1>{forum?.name} </h1>
-        {forum?.subForums && forum.subForums.length > 0 ?(
-            forum.subForums.map((sub) =>(
-            <div className="subForumList" key={sub.id}>
-                 <p onClick={()=> handleSubForum(sub)}>{sub?.name}</p>
+    <div className="AllForum">
 
-                <div className="subChats">
-                    {sub?.subChats && sub.subChats.length > 0 ? (
-                        sub.subChats.map((chats)=>(
-                            <div className="subChatsList" key={chats.id}> 
-                                 <p onClick={()=> handleChatName(chats)}> {chats.name}</p> 
-                            </div>
+
+    
+        <div className="subForums">
+            <h2>{forum?.name} </h2>
+            <hr/>
+            {forum?.subForums && forum.subForums.length > 0 ?(
+                forum.subForums.map((sub) =>(
+                <div className="subForumList" key={sub.id}>
+                    <h3 onClick={()=> handleSubForum(sub)}>{sub?.name}</h3>
+
+                    <div className="subChats">
+                        {sub?.subChats && sub.subChats.length > 0 ? (
+                            sub.subChats.map((chats)=>(
+                                <div className="subChatsList" key={chats.id}> 
+                                    <p onClick={()=> handleChatName(chats)}> {chats.name}</p> 
+                                </div>
                            
-                        ))
-                    ): (<p> No hi ha cap chat </p>)}
+                            ))
+                        ): (<p> No hi ha cap chat </p>)}
+                    </div>
                 </div>
-            </div>
             
            
-        ))
-        ):(<p>No hi ha subforums</p>)}
-       
-    </div>
-
-    <div className="principalBody">
-        {!actualChat && !actualSubForum && (
-
-       <div className="ForumDescription"> 
-            <h1> Benvolguts a {forum?.name}</h1>
-            <h3>{forum?.description}</h3>
-        </div>
-    
-        )}
-    
-        {actualChat && (
-            <div className="ForumDescription"> 
-                <p>{actualChat.name}</p>
-                {actualChat.listMessages.length >0 &&(
-
-                    actualChat.listMessages.map((messages)=>(
-                    <div className="messages" key={messages.id}>
-                        <p>{messages.content}</p>
-                    </div>
-                    
                 ))
-                
-                )}
-            </div>
-        )}
-        {actualSubForum && (
-            <div className="ForumDescription"> 
-                <h2>{actualSubForum.name}</h2>
-                <p>{actualSubForum.description}</p>
-            </div>
-        )}
+            ):(<p>No hi ha subforums</p>)}
+       
+        </div>
 
+        <div className="principalBody">
+            {!actualChat && !actualSubForum && (
+
+            <div className="ForumDescription"> 
+                
+                <h1 > Benvolguts a {forum?.name}</h1>
+                <h3>{forum?.description}</h3>
+            </div>
+    
+            )}
+    
+            {actualChat && (
+                <div className="chatInterface"> 
+                    <h2 className="chatName">{actualChat.name}</h2>
+                    {actualChat.listMessages.length >0 &&(
+
+                        actualChat.listMessages.map((messages)=>(
+                        <div className="messages" key={messages.id}>
+                            <p className="singleMessage">{messages.content}</p>
+                        </div>
+                    
+                        ))
+                
+                    )}
+                </div>
+            )}
+            {actualSubForum && (
+                <div className="ForumDescription"> 
+
+                    <h2 className="subForumName">{actualSubForum?.name}</h2>
+                    <h2>{actualSubForum.name}</h2>
+                    <p>{actualSubForum.description}</p>
+                </div>
+            )}
+
+        </div>
     </div>
 
     </>
